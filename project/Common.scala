e8f3cbffdd60ff30886f5deaa8570e4df6be0f54
Mark Tranter <mark.a.tranter@gmail.com>
2019-05-23 08:52:58 +1000

    Use docker in build

--- project/Common.scala
+++ project/Common.scala
@@ -3,7 +3,8 @@ import sbt.URL
 
 object Common {
   def apply() = Seq(
-    scalaVersion := "2.12.6",
+    scalaVersion := "2.12.8",
+    organization := "com.engitano",
     organizationName := "Engitano",
     startYear := Some(2019),
     licenses += ("MIT", new URL("http://opensource.org/licenses/MIT"))
